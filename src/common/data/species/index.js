ba1fdec6c4169fb1bfa683c6f22b320cb33e9c55
Karolis Kazlauskis <karolis@flumens.io>
2021-02-25 18:42:21 +0000

    Improve image loader

--- src/common/data/species/index.js
+++ src/common/data/species/index.js
@@ -6,11 +6,29 @@ import './photos'; // webpack-loading only
 const extendWithResources = sp => {
   const bySpeciesID = ({ speciesID }) => speciesID === sp.id;
 
+  const removeJpg = photo => ({
+    ...photo,
+    ...{ file: photo.file.replace('.jpg', '') },
+  });
+
+  const images = photos.filter(bySpeciesID).map(removeJpg);
+
   Object.assign(sp, speciesResources[sp.id], {
-    images: photos.filter(bySpeciesID),
+    images,
   });
 };
 
 species.forEach(extendWithResources);
+
+/* After adding new images run this script to quick-check all are present */
+// (async () => {
+//   for (let spIndex = 0; species.length > spIndex; spIndex++) {
+//     for (let index = 0; species[spIndex].images.length > index; index++) {
+//       console.log(`/images/${species[spIndex].images[index].file}.jpg`);
+//       await fetch(`/images/${species[spIndex].images[index].file}.jpg`);
+//       await new Promise(resolve => setTimeout(resolve, 20));
+//     }
+//   }
+// })();
 
 export default species;
