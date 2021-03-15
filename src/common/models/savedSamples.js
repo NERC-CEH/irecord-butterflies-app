c27ba559b84e90073b6950ddd0c77598fc695a98
Karolis Kazlauskis <karolis@flumens.io>
2021-03-15 11:53:20 +0000

    Remove auto-upload

--- src/common/models/savedSamples.js
+++ src/common/models/savedSamples.js
@@ -1,36 +1,7 @@
 import { initStoredSamples } from '@apps';
-import { reaction } from 'mobx';
 import { modelStore } from './store';
 import Sample from './sample';
-import userModel from './user';
 
 const savedSamples = initStoredSamples(modelStore, Sample);
-
-function uploadAllFinished() {
-  console.log('SavedSamples: uploading all surveys');
-
-  const uploadIfFinished = sample => {
-    const isAlreadyUploaded = !!sample.id;
-    if (isAlreadyUploaded || !sample.metadata.saved) {
-      return;
-    }
-    const invalids = sample.validateRemote();
-    if (invalids) {
-      return;
-    }
-    sample.saveRemote();
-  };
-  savedSamples.forEach(uploadIfFinished);
-}
-
-const uploadFinishedSurveysAndListenForLogin = () => {
-  const uploadIfLoggedIn = isLoggedIn => isLoggedIn && uploadAllFinished();
-  uploadIfLoggedIn(userModel.attrs.id);
-
-  const listenToUserLogin = () => userModel.attrs.id;
-  reaction(listenToUserLogin, uploadIfLoggedIn);
-};
-
-savedSamples._init.then(uploadFinishedSurveysAndListenForLogin);
 
 export default savedSamples;
