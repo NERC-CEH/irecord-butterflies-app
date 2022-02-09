97a44a4c15ae8578029a0bbc28629b6289a75e01
Vilius Stankaitis <vilius@stankaitis.com>
2022-02-09 13:34:34 +0200

    #5 Add day-flying moths to species list and guide

--- src/common/data/species/index.ts
+++ src/common/data/species/index.ts
@@ -2,6 +2,7 @@ import species from './cache/species.json';
 import * as resources from './other';
 import photos from './cache/photos.json';
 import './photos'; // webpack-loading only
+import './photos-moth'; // webpack-loading only
 
 interface Species {
   id: string;
@@ -71,10 +72,14 @@ const extendedSpecies = (species as Species[]).map(extendWithResources);
 
 /* After adding new images run this script to quick-check all are present */
 // (async () => {
-//   for (let spIndex = 0; species.length > spIndex; spIndex++) {
-//     for (let index = 0; species[spIndex].images.length > index; index++) {
-//       console.log(`/images/${species[spIndex].images[index].file}.jpg`);
-//       await fetch(`/images/${species[spIndex].images[index].file}.jpg`);
+//   for (let spIndex = 0; extendedSpecies.length > spIndex; spIndex++) {
+//     for (
+//       let index = 0;
+//       extendedSpecies[spIndex]?.images?.length > index;
+//       index++
+//     ) {
+//       console.log(`/images/${extendedSpecies[spIndex].images[index].file}.jpg`);
+//       await fetch(`/images/${extendedSpecies[spIndex].images[index].file}.jpg`);
 //       await new Promise(resolve => setTimeout(resolve, 20));
 //     }
 //   }
---
+++ src/common/data/species/photos-moth/index.js
@@ -0,0 +1,67 @@
+/**
+ * Use this to process all:
+ */
+// mogrify -verbose -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -resize 1024 -format jpg */*
+
+/**
+ * Generate this list:
+ */
+// cd src/common/data/species/photos && ls */*
+
+import './AntlerMoth/Antler Moth (Iain Leach).jpg?originalName';
+import './BeautifulYellowUnderwing/Beautiful Yellow Underwing (Keith Tailby).jpg?originalName';
+import './BeautifulYellowUnderwing/Beautiful Yellow Underwing larva (Bob Eade).jpg?originalName';
+import './BorderedWhite/Bordered White (Iain Leach).jpg?originalName';
+import './BrownSilverline/Brown Silver-line (Keith Tailby).jpg?originalName';
+import './BurnetCompanion/Burnet Companion (Patrick Clement).jpg?originalName';
+import './ChimneySweeper/Chimney Sweeper (Bob Eade).jpg?originalName';
+import './Cinnabar/Cinnabar (Bob Eade).jpg?originalName';
+import './Cinnabar/Cinnabar (Mark Parsons).jpg?originalName';
+import './Cinnabar/Cinnabar larva (Dan Lombard).jpg?originalName';
+import './CloudedBuff/Clouded Buff male (Dave Green).jpg?originalName';
+import './CommonCarpet/Common Carpet (Keith Tailby).jpg?originalName';
+import './CommonHeath/Common Heath female (Patrick Clement).jpg?originalName';
+import './CommonHeath/Common Heath male (Bob Eade).jpg?originalName';
+import './CommonHeath/Common Heath male (Patrick Clement).jpg?originalName';
+import './EmperorMoth/Emperor Moth female (Iain Leach).jpg?originalName';
+import './EmperorMoth/Emperor Moth larva (Bob Eade).jpg?originalName';
+import './EmperorMoth/Emperor Moth male (Iain Leach).jpg?originalName';
+import './EmperorMoth/Emperor Moth young larva (Dan Lombard).jpg?originalName';
+import './FoxMoth/Fox Moth female (Bob Eade).jpg?originalName';
+import './FoxMoth/Fox Moth larva (Bob Eade).jpg?originalName';
+import './FoxMoth/Fox Moth male (Dave Green).jpg?originalName';
+import './FoxMoth/Fox Moth young larva (Dan Lombard).jpg?originalName';
+import './GoldSwift/Gold Swift (Dan Lombard).jpg?originalName';
+import './GoldSwift/Gold Swift (Iain Leach).jpg?originalName';
+import './GrassRivulet/Grass Rivulet (Patrick Clement).jpg?originalName';
+import './HummingbirdHawkmoth/Humming-bird Hawk-moth (David Green).jpg?originalName';
+import './HummingbirdHawkmoth/Humming-bird-hawk-moth (Bob Eade)3.jpg?originalName';
+import './JerseyTiger/Jersey Tiger (Bob Eade).jpg?originalName';
+import './JerseyTiger/Jersey Tiger (Peter Maton).jpg?originalName';
+import './LatticedHeath/Latticed Heath (Chris Manley)2.jpg?originalName';
+import './LatticedHeath/Latticed Heath (Keith Tailby).jpg?originalName';
+import './MagpieMoth/Magpie Moth (Patrick Clement).jpg?originalName';
+import './MotherShipton/Mother Shipton (Chris Manley).jpg?originalName';
+import './MotherShipton/Mother Shipton (Patrick Clement).jpg?originalName';
+import './OakEggar/Oak Eggar female (Iain Leach).jpg?originalName';
+import './OakEggar/Oak Eggar larva (Bob Eade)2.jpg?originalName';
+import './OakEggar/Oak Eggar male (Tim Melling).jpg?originalName';
+import './ScarletTiger/Scarlet Tiger (Bob Eade).jpg?originalName';
+import './ScarletTiger/Scarlet Tiger (Patrick Clement).jpg?originalName';
+import './ScarletTiger/Scarlet Tiger larva (Patrick Clement).jpg?originalName';
+import './ShadedBroadbar/Shaded Broad-bar (Mark Parsons).jpg?originalName';
+import './SilverY/Silver Y (Iain Leach).jpg?originalName';
+import './SilverY/Silver Y (Iain Leach)2.jpg?originalName';
+import './SilvergroundCarpet/Silver-ground Carpet (Keith Tailby).jpg?originalName';
+import './SixspotBurnet/Six-spot Burnet + cocoon (Bob Eade).jpg?originalName';
+import './SixspotBurnet/Six-spot Burnet underwing (Bob Eade).jpg?originalName';
+import './SmallPurplebarred/Small Purple-barred (Bob Eade).jpg?originalName';
+import './SmallPurplebarred/Small Purple-barred (Keith Tailby).jpg?originalName';
+import './SmallYellowUnderwing/Small Yellow Underwing (Keith Tailby).jpg?originalName';
+import './SpeckledYellow/Speckled Yellow (Bob Eade)2.jpg?originalName';
+import './SpeckledYellow/Speckled Yellow (Iain Leach).jpg?originalName';
+import './VapourerMoth/Vapourer female and eggs (Patrick Clement).jpg?originalName';
+import './VapourerMoth/Vapourer larva (Iain Leach).jpg?originalName';
+import './VapourerMoth/Vapourer male (Iain Leach).jpg?originalName';
+import './YellowShell/Yellow Shell (Peter Maton).jpg?originalName';
+import './YellowShell/Yellow Shell (Peter Maton)2.jpg?originalName';
