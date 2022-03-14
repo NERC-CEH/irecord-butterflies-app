027b6d67ac2ec82e5ed509660cd41f6fe65bbd61
Vilius Stankaitis <vilius@stankaitis.com>
2022-03-14 13:55:56 +0200

    #128 Fix Timed count Duration after 60 minutes

--- src/common/helpers/getFormattedDuration.ts
+++ src/common/helpers/getFormattedDuration.ts
@@ -1,6 +1,7 @@
+const MINUTE = 60000;
 function getFormattedDuration(duration: number): string {
   const seconds = new Date(duration).getSeconds();
-  const minutes = new Date(duration).getMinutes();
+  const minutes = Math.floor(duration / MINUTE);
   const formattedSeconds = seconds > 9 ? seconds : `0${seconds}`;
   const formattedMinutes = minutes > 9 ? minutes : `0${minutes}`;
 
