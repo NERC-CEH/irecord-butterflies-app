75321e7cf22809a6fd6e423e3ab353c0e9a33801
Karolis Kazlauskis <karolis@flumens.io>
2022-07-02 07:07:30 +0300

    *250 Fix virtual list

--- src/common/Components/VirtualList.tsx
+++ src/common/Components/VirtualList.tsx
@@ -1,13 +1,18 @@
 import { FC, useEffect, useState, useRef } from 'react';
 import { FixedSizeList as List } from 'react-window';
+import { useIonViewDidEnter } from '@ionic/react';
 
 // eslint-disable-next-line react/no-unstable-nested-components
 const VirtualList: FC<any> = ({ itemSize, Item, topPadding = 0, ...props }) => {
   const contentRef = useRef<any>();
   const [listHeight, setListHeight] = useState<number>(1); // some positive number
 
-  const setCurrentContentHeight = () =>
-    contentRef.current && setListHeight(contentRef.current.clientHeight);
+  const setCurrentContentHeight = () => {
+    if (listHeight <= 1 && contentRef?.current?.clientHeight) {
+      setListHeight(contentRef.current.clientHeight);
+    }
+  };
+  useIonViewDidEnter(setCurrentContentHeight); // before mounting the first time list has no height
   useEffect(setCurrentContentHeight, [contentRef.current]);
 
   // eslint-disable-next-line react/no-unstable-nested-components
